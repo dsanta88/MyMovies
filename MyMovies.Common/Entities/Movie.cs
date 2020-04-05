b84aefdf6c3294290fad26c0d72955b89caa03f8
santadavid88@hotmail.com <k0l0mbia>
2020-04-05 16:22:46 -0500

    Crud Movies

---
+++ MyMovies.Common/Entities/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MyMovies.Common.Entities
+{
+   public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public bool InTheater { get; set; }
+
+        [Required]
+        public DateTime? DatePremiere { get; set; }
+        public string Trailer { get; set; }
+        public string TitleShort { get; set; }
+        public string Poster { get; set; }
+    }
+}
