9918279007c6f48db9a4212fe8d96174c2d92559
santadavid88@hotmail.com <k0l0mbia>
2020-04-05 11:39:06 -0500

    Search Movies

--- MyMovies.Common/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MyMovies.Common
-{
-    public class Class1
-    {
-    }
-}
--- MyMovies.Common/Entities/Person.cs
+++ MyMovies.Common/Entities/Person.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace MyMovies.Common.Entities
 {
-    public class Category
+  public  class Person
     {
         public int Id { get; set; }
-
-        [Required(ErrorMessage = "El campo {0} es requerido")]
         public string Name { get; set; }
+        public string Photo { get; set; }
+        public string Bibliography { get; set; }
+        public DateTime DateBirth { get; set; }
+
     }
 }
-
-
