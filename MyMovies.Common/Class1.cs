90f81e2fe0c9a058ae1a6be08e18a665cd1b4bf2
santadavid88@hotmail.com <k0l0mbia>
2020-04-05 10:40:10 -0500

    Crud Categories

---
+++ MyMovies.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MyMovies.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ MyMovies.Common/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MyMovies.Common.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string Name { get; set; }
+    }
+}
